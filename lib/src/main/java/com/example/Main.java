6dc50e24fb45f7a0733197819d6d9537b4d8041f
brank12345 <brank12345@gmail.com>
2016-06-12 21:48:36 +0800

    finish

---
+++ lib/src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Created by brank12345 on 16/6/12.
+ */
+public class Main {
+    public static void main(String[] args){
+        Server obj = new Server();
+    }
+}
